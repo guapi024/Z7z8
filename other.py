6d441f8ac94594732f895c6203e2164eaf734f04
renou <renou_han@hotmail.com>
2017-12-04 09:36:43 +0800

    master

---
+++ other.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+__author__ : renou
+__file_name__ : other.py 
+__time__ : 2017-11-28 
+'''
+import  platform
+print platform.architecture()
+print platform.platform()
+print platform.system()
+print platform.python_version()
