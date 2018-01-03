98a529e9c48936a7439c24bffd7545b987040e42
renou <renou_han@hotmail.com>
2018-01-03 20:41:13 +0800

    master

--- tools/send_wechat.py
+++ tools/send_wechat.py
@@ -25,11 +25,10 @@ class main(object):
                 ini_dict[i] = dict(ci)
         except ConfigParser.ParsingError, e:
             print u"error is %s" % e
-
         finally:
             return ini_dict
 
-    def get_token(corpid, corpsecret):
+    def get_token(self,corpid, corpsecret):
         try:
             wx_url_get = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
             wx_login = {'corpid': corpid,
@@ -39,7 +38,8 @@ class main(object):
         except Exception, e:
             return json.loads(wx_get.text)['errmsg']
 
-    def post_msg(token, msg):
+    def post_msg(self,token, msg):
+        print token,msg
         try:
             msg = json.dumps(msg, ensure_ascii=False)
             wx_url_post = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s' % (token)
@@ -47,12 +47,37 @@ class main(object):
             return wx_post.text
         except Exception, e:
             print  'error is ', e, token
-    def other(self):
-        pass
+    def msg(self,data,msgdata,msgtype):
+        msg={}
+        msg['touser'] = data['touser']
+        msg['toparty'] = data['toparty']
+        msg['totag'] = data['totag']
+        msg['agentid'] = data['agentid']
+        msg['safe'] = data['safe']
+        msg['msgtype'] = msgtype
+        if  msgtype=='text':
+            msg['content']=msgdata
+        return msg
+    def start(self,cnf,msgdata,msgtype):
+        wechat_cnf=self.load_conf(cnf)['wechat']
+        # print wechat_cnf
+        access_token=wechat_cnf['access_token']
+        if  access_token:
+            access_token=access_token
+        else:
+            access_token=self.get_token(wechat_cnf['corpid'],wechat_cnf['corpsecret'])
+        msgdata=self.msg(wechat_cnf,msgdata,msgtype)
+        print msgdata
 
-ss=main()
-confpath='conf.ini'
-print ss.load_conf(confpath)
 
 
-# if __name__ == '__main__':
+
+
+
+
+if __name__ == '__main__':
+    ss = main()
+    confpath = 'conf.ini'
+    msgdata='test 123 '
+    msgtype='text'
+    ss.start(confpath,msgdata,msgtype)
