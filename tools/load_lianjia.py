1723adc98b28b92c2e349655cde782bf80898e9d
renou <renou_han@hotmail.com>
2018-03-21 22:43:49 +0800

    master

--- tools/load_lianjia.py
+++ tools/load_lianjia.py
@@ -349,7 +349,6 @@ if __name__ == '__main__':
 
         es_url = "https://bj.lianjia.com/ershoufang/"
 
-
         if len(sys.argv)>=2:
             es_url = sys.argv[1]
         file_dt = '_'.join(es_url.split("/")[2:4]).replace(".lianjia.com", "") + "_" + str(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f_%p'))
