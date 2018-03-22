11007b9a6b8f906ab08059f532c06f4aa675cb5f
renou <renou_han@hotmail.com>
2018-03-23 06:26:31 +0800

    master

--- tools/load_lianjia.py
+++ tools/load_lianjia.py
@@ -349,7 +349,7 @@ if __name__ == '__main__':
 
         es_url  =   "https://sh.lianjia.com/ershoufang/"
 
-        es_url =    "https://bj.lianjia.com/ershoufang/"
+        # es_url =    "https://bj.lianjia.com/ershoufang/"
 
         if len(sys.argv)>=2:
             es_url = sys.argv[1]
